b59c0ad7b8f01536d0e3a72b641bc8d4b81be7b8
Igor Oliinyk <mrfinansist1@gmail.com>
2019-05-25 16:55:08 +0300

    mainTask: 3 task done

---
+++ mainTask/src/task1.js
@@ -0,0 +1,4 @@
+export default function buildChessBoard() {
+    console.log('2');
+}
+
---
+++ mainTask/src/task6.js
@@ -0,0 +1,19 @@
+export default function findArrNumbers(numLength, square) {
+    if (typeof numLength === 'number' &&
+        typeof square === 'number' &&
+        numLength > 0 &&
+        square > 0) {
+        const arrNumbers = [];
+        for (let number = 0; arrNumbers.length < numLength; number++) {
+            if ((number * number) > square) {
+                arrNumbers.push(number);
+            }
+        }
+        return (arrNumbers.join(','));
+    } else {
+        return ({
+            status: 'failed',
+            reason: 'Input only integers as numLength and square',
+        })
+    }
+}
