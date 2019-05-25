413e56dc3045c03cf377aee73c565b2155e976eb
Igor Oliinyk <mrfinansist1@gmail.com>
2019-05-25 21:26:48 +0300

    mainTask: add task1,2

--- mainTask/main.js
+++ mainTask/main.js
@@ -1,21 +1,36 @@
-import buildChessBoard from './src/task1.js';
-/* import buildChessBoard from './src/task2.js';*/
+import chessBuilder from './src/task1.js';
+import putEnvelope from './src/task2.js';
 import sortTriangles from './src/task3.js';
-import checkPalindrom from './src/task4.js';
+/* import checkPalindrom from './src/task4.js'; */
 import findLuckyTicket from './src/task5.js';
 import findArrNumbers from './src/task6.js';
 /*import buildChessBoard from './src/task7.js'; */
 
 /* DONE
+task1(need to add checkup)
+task2(need to add checkup)
+task3(need to add checkup)
 task5
 task6
 */
 /* checkPalindrom(1234437); */
+chessBuilder(5, 4, '+');
+
+putEnvelope({
+    a: 2,
+    b: 20
+}, {
+    c: 2,
+    d: 2
+})
+
 findArrNumbers(30, 400);
+
 findLuckyTicket({
     min: 311133,
     max: 411112
-})
+});
+
 sortTriangles([{
         vertices: 'ABC',
         a: 10,
@@ -28,4 +43,4 @@ sortTriangles([{
         b: 24.33,
         c: 26
     }
-])
+]);
--- mainTask/src/task2.js
+++ mainTask/src/task2.js
@@ -1,4 +1,11 @@
-export default function buildChessBoard() {
-    console.log('2');
+function putEnvelope(envelope1 = {}, envelope2 = {}) {
+    if (envelope1.a > envelope2.c &&
+        envelope1.b > envelope2.d) {
+        return 'envelope1';
+    } else if (envelope1.a < envelope2.c &&
+        envelope1.b < envelope2.d) {
+        return 'envelope2';
+    } else {
+        return '0';
+    }
 }
-
